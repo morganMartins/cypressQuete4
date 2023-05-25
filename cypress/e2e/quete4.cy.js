2ee779d1f90669775b4ae0780f1c991b0dedbbb9
morganMartins <130161456+morganMartins@users.noreply.github.com>
2023-05-25 11:24:05 +0200

    nouveau projet

---
+++ cypress/e2e/quete4.cy.js
@@ -0,0 +1,43 @@
+
+describe("File upload and download tests", () => {
+  beforeEach(() => {
+    cy.visit("https://filebin.net/");
+   // cy.downloadFile('https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg','cypress/fixtures','fileToUpload.jpg')
+  });
+
+  it("Upload file and download it in Zip format", () => {
+    cy.get("#fileField").attachFile("cypress/fixtures/fileToUpload.jpg");
+    cy.contains("It contains 1 uploaded file").should("be.visible");
+    cy.contains("Download files").click();
+    cy.contains("Zip")
+      .invoke("attr", "href")
+      .then((downloadLink) => {
+        const absulteLink = "https://filebin.net/" + downloadLink;
+        cy.log(absulteLink);
+        cy.downloadFile(
+          absulteLink,
+          "mydownloads/zipFiles",
+          "downloadedFromCypress.zip"
+        );
+        cy.readFile("mydownloads/zipFiles/downloadedFromCypress.zip");
+      });
+  });
+
+  it("Upload file and download it in Tar format", () => {
+    cy.get("#fileField").attachFile("cypress/fixtures/fileToUpload.jpg");
+    cy.contains("It contains 1 uploaded file").should("be.visible");
+    cy.contains("Download files").click();
+    cy.contains("Tar")
+      .invoke("attr", "href")
+      .then((downloadLink) => {
+        const absulteLink = "https://filebin.net/" + downloadLink;
+        cy.log(downloadLink);
+        cy.downloadFile(
+          absulteLink,
+          "mydownloads/tarFiles",
+          "downloadedFromCypress.tar"
+        );
+        cy.readFile("mydownloads/tarFiles/downloadedFromCypress.tar");
+      });
+  });
+});
